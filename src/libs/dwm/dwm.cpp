8b7862830715012feddf2c7f067b939f825aa29b
Deano Calver <deano@cloudpixies.com>
2012-04-15 19:51:36 +0100

    fix for not using stdc11 on linux Dwm lib created (Distributed World Machine) by default don't recurse in submodules

---
+++ src/libs/dwm/dwm.cpp
@@ -0,0 +1,16 @@
+/*
+ * dwm.cpp
+ *
+ *  Created on: 15 Apr 2012
+ *      Author: deanoc
+ */
+#include "dwm.h"
+
+void Dwm::bootstrapLocal(){
+	auto hwThreads = Core::thread::hardware_concurrency();
+	auto thread0 = new Core::thread();
+
+}
+
+
+
---
+++ src/libs/dwm/dwm.h
@@ -0,0 +1,21 @@
+/*
+ * dwm.h
+ *
+ *  Created on: 15 Apr 2012
+ *      Author: deanoc
+ */
+
+#ifndef YOLK_DWM_DWM_H_
+#define YOLK_DWM_DWM_H_
+
+#include "core/core.h"
+
+
+class Dwm {
+public:
+	void bootstrapLocal();
+private:
+};
+
+
+#endif /* DWM_H_ */
