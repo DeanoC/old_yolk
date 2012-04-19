aac7d4d91cc81f97267b26549c7edcaaa8b8b6d7
Deano Calver <deano@cloudpixies.com>
2012-04-19 15:53:19 +0100

    riak-cpp library in and compiling protobuf fit in nicely (need relative path for PROTOBUF_GENERATE_CPP now though) (riak uses)

--- cmake_modules/FindWierdProtobuf.cmake
+++ cmake_modules/FindWierdProtobuf.cmake
@@ -9,15 +9,14 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS)
   set(${HDRS})
   foreach(FIL ${ARGN})
     get_filename_component(ABS_FIL ${FIL} ABSOLUTE)
-    get_filename_component(FIL_WE ${FIL} NAME_WE)
     
-    list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.pb.cc")
-    list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.pb.h")
+    list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL}.pb.cc")
+    list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${FIL}.pb.h")
 
     add_custom_command(
-      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.pb.cc"
-             "${CMAKE_CURRENT_BINARY_DIR}/${FIL_WE}.pb.h"
-      COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
+      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FIL}.pb.cc"
+             "${CMAKE_CURRENT_BINARY_DIR}/${FIL}.pb.h"
+      COMMAND  protoc
       ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR} --proto_path ${CMAKE_CURRENT_SOURCE_DIR} ${ABS_FIL}
       DEPENDS ${ABS_FIL}
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
@@ -29,61 +28,4 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS)
   set(${HDRS} ${${HDRS}} PARENT_SCOPE)
 endfunction()
 
-FIND_PATH( Protobuf_INCLUDE_DIRS "google/protobuf/service.h"
-	PATHS
-		"${CMAKE_CURRENT_SOURCE_DIR}/third/"
-	DOC
-		"The Google Protocol Buffers Include Directory"
-)
-find_program( PROTOBUF_PROTOC_EXECUTABLE NAMES protoc
-		DOC 
-			"The Google Protocol Buffers Compiler"
-		PATHS
-			"${CMAKE_CURRENT_SOURCE_DIR}/third/google/protobuf/bin"
-				
-)
-
-# Google's provided vcproj files generate libraries with a "lib"
-# prefix on Windows
-if(WIN32)
-    set(PROTOBUF_ORIG_FIND_LIBRARY_PREFIXES "${CMAKE_FIND_LIBRARY_PREFIXES}")
-    set(CMAKE_FIND_LIBRARY_PREFIXES "lib" "")
-endif()
-
-SET( PROTOBUF_PATH  "${CMAKE_CURRENT_SOURCE_DIR}/third/google/protobuf/lib" )
-IF( IS_HOST_64_BIT )
-	SET( PROTOBUF_PATH "${PROTOBUF_PATH}/x64" )
-ELSE()
-	SET( PROTOBUF_PATH "${PROTOBUF_PATH}/x86" )
-ENDIF()
-
-FIND_LIBRARY(Protobuf_LIBRARY_RELEASE 
-	protobuf${_host_COMPILER}
-	PATHS
-		${PROTOBUF_PATH}
-)
-FIND_LIBRARY(Protobuf_LIBRARY_DEBUG 
-	protobuf${_host_COMPILER}_d
-	PATHS
-		${PROTOBUF_PATH}
-)
-FIND_LIBRARY(Protobuf-lite_LIBRARY_RELEASE 
-	protobuf-lite${_host_COMPILER}
-	PATHS
-		${PROTOBUF_PATH}
-)
-FIND_LIBRARY(Protobuf-lite_LIBRARY_DEBUG 
-	protobuf-lite${_host_COMPILER}_d
-	PATHS
-		${PROTOBUF_PATH}
-)
-
-# Restore original find library prefixes
-if(WIN32)
-    set(CMAKE_FIND_LIBRARY_PREFIXES "${PROTOBUF_ORIG_FIND_LIBRARY_PREFIXES}")
-endif()
-
 SET( Protobuf_FOUND 1 )
-SET( Protobuf_LIBRARIES debug ${Protobuf_LIBRARY_DEBUG} optimized ${Protobuf_LIBRARY_RELEASE} )
-SET( Protobuf-lite_LIBRARIES debug ${Protobuf-lite_LIBRARY_DEBUG} optimized ${Protobuf-lite_LIBRARY_RELEASE} )
-MARK_AS_ADVANCED(Protobuf_INCLUDE_DIRS Protobuf-lite_LIBRARY_DEBUG Protobuf-lite_LIBRARY_RELEASE Protobuf_LIBRARY_DEBUG Protobuf_LIBRARY_RELEASE PROTOBUF_PATH )
